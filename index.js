01da54e14d71f6848822733bbac2678721aaf153
Will Smith <wismith@davidson.edu>
2020-02-26 16:59:23 -0800

    following medium article's guidelines

--- index.js
+++ index.js
@@ -10,8 +10,25 @@ const port = 3000;
 // Serve static files (needed to import CSS file)
 app.use(express.static('public'));
 
-// Set a basic route
-app.get('/', (req, res) => res.send('Hello World!'));
+// Load handlebars module
+const handlebars = require('express-handlebars');
+
+// Set our app to use handlebars engine
+app.set('view engine', 'hbs');
+
+// Set handlebars configurations
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'planB'
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  // Serves the body of the page to the container
+  res.render('main', {layout: 'index'});
+});
 
 // Make the app listen to port 3000
 app.listen(port, () => console.log(`App listening to port ${port}`));
